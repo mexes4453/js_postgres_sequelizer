75ecfa368bf96a66c7da8f5c475c4ad4a4f1cb42
cudoh <cudoh@student.42wolfsburg.de>
2025-09-01 23:14:05 +0200

    tut05_v9: p2 -> how sequelize getter, setter and virtual works

--- models/user.js
+++ models/user.js
@@ -28,10 +28,23 @@ const User = sequelize.define("User", {
         set(value){
             this.setDataValue("email", value.toLowerCase());
         }
-    }
+    },
+    /*
+    birthdate:{ // Used to demonstrate virtuals 
+        type: DataTypes.DATE
+    }*/
+
 });
+/*
+User.prototype.getAge = function(){
+    const birthdate = this.getDataValue("birthdate");
+    if (!birthdate) return null;
 
-
+    const today = new Date();
+    const age = today.getFullYear - birthdate.getFullYear();
+    return age;
+}
+*/
 
 /*>
  * An anonymous functions defined and called at the same time to test
@@ -45,21 +58,29 @@ const User = sequelize.define("User", {
      * create a new user and note that the fullname is not provided because
      * it is declared as virtual in the model definition. */
     const user = await User.create({
-         firstName: "Gehard",
-         lastName: "Jahn",
-         email:"Gerhard.Jahn@d.com"});
+        firstName: "cehard",
+        lastName: "John",
+        email:"Gerhard.Jahn@d.com",
+        //birthdate: "1990-01-01"
+    });
 
     /*>
      * Attempting to access the virtual attribute (fullName) triggers the call
      * to its getter function defined in the model */
     console.log( user.fullName); 
 
-    /* The email setter will convert the all characters in the email to lower
+    /*>
+     * The email setter will convert the all characters in the email to lower
      * case before setting it in the database.
      * Therefore, when the email attribute is accessed. it will print the 
-     * email is lowercase eventhough user provide email that contains upper
+     * email in lowercase eventhough email provided by user contains upper
      * case characters.*/
     console.log( user.email);
+
+    /*>
+     * This call will calculate the age of the user and print it on the screen
+     * but this age is not saved in the database */
+    //console.log(user.getAge()); 
 })();
 
 module.exports = User;
