a643234b6d4b2ea11e74a7234171536c33c9d5a0
cudoh <cudoh@student.42wolfsburg.de>
2025-09-01 05:41:53 +0200

    tut05_v9: p0 -> how sequelize getter, setter and virtual works

--- models/user.js
+++ models/user.js
@@ -10,6 +10,14 @@ const User = sequelize.define("User", {
         type: DataTypes.STRING,
         allowNull: false
     },
+    fullName: {
+        type:DataTypes.VIRTUAL,  // declare a virtual attribute
+        get(){
+            const firstName = this.getDataValue('firstName')
+            const lastName = this.getDataValue('lastName')
+            return (`${firstName} - ${lastName}`)
+        }
+    },
     email: {
         type: DataTypes.STRING,
         unique:true,
@@ -18,7 +26,28 @@ const User = sequelize.define("User", {
             isEmail: true
         }
     }
-    
 });
+
+/*>
+ * An anonymous functions defined and called at the same time to test
+ * the usage of getters, and virtual attribute */
+(async ()=>
+{
+    await sequelize.sync();
+    console.log("TEST_USER: GETTER AND VIRTUALS ")
+
+    /*>
+     * create a new user and note that the fullname is not provided because
+     * it is declared as virtual in the model definition. */
+    const user = await User.create({
+         firstName: "Gehard",
+         lastName: "Jahn",
+         email:"gj@d.com"});
+
+    /*>
+     * Attempting to access the virtual attribute (fullName) triggers the call
+     * to its getter function defined in the model */
+    console.log( user.fullName); 
+})();
 
 module.exports = User;
