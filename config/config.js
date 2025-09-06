824d6ee130113aefedad5a1655734af1bf4fb4e2
cudoh <cudoh@student.42wolfsburg.de>
2025-09-06 10:32:51 +0200

    tut11_v21: migration node with sequelize

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
-{
+require('dotenv').config(); // Load .env
+const appEnv = process.env;
+
+module.exports  = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": appEnv.DB_USER,
+    "password": appEnv.DB_PASSWORD,
+    "database": appEnv.DB_NAME,
+    "host": appEnv.DB_HOST,
+    "dialect": appEnv.DB_TYPE
   },
   "test": {
     "username": "root",
